4ef444f7719c541b90388a5806d87d7ad6d546cf
Travis van der Font <travis.font@gmail.com>
2023-06-04 23:13:27 +0200

    initial test: testBasicNewArrayList()

---
+++ tests/ConstructTest.php
@@ -0,0 +1,15 @@
+<?php
+
+use Qsl\ArrayList;
+use PHPUnit\Framework\TestCase;
+
+class ConstructTest extends TestCase
+{
+    public function testBasicNewArrayList(): void
+    {
+        $expected = [21, 42, 64, 64];
+        $actual = new ArrayList(21, 42, 64, 64);
+
+        $this->assertSame($expected, $actual->toArray());
+    }
+}
