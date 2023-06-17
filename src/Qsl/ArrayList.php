de62b0d5cd3a02c10167f1024daf00759a105b0c
Travis van der Font <travis.font@gmail.com>
2023-06-17 23:13:52 +0200

    Cloneable interface

--- src/Qsl/ArrayList.php
+++ src/Qsl/ArrayList.php
@@ -2,7 +2,17 @@
 
 namespace Qsl;
 
-class ArrayList
+interface Cloneable
+{
+    public function add(int);
+    public function delete(int); // remove(); // clear()
+    public function contains(): bool;
+    public function equals(); // Compares this object against the specified object.
+    public function hashCode(); // Gets the hashcode.
+    public function toString()(); // Converts the data set to a String.
+}
+
+class ArrayList implements Cloneable
 {
     private array $_list;
 
